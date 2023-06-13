b66d058d0d983e86cca959d1de985b36c120fae4
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-13 17:01:14 +0200

    elements get

--- js/snack1.js
+++ js/snack1.js
@@ -1,2 +1,23 @@
 //JS RESET
 console.log('JS OK')
+
+//! OPERAZIONI INIZIALI:
+
+//todo PRENDO GLI ELEMENTI INTERESSATI:
+const guestList = [
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
+  ];
+const tableName = 'Tavolo VIP';
+const placeCards = [];
+
+//! LOGICA EFFETTIVA:
+
