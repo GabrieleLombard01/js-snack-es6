707219feb1ba2ce82dce76cb347a879ec195fabb
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-14 16:02:39 +0200

    array added

--- js/snack3.js
+++ js/snack3.js
@@ -3,4 +3,15 @@ console.log('JS OK')
 
 //! OPERAZIONI INIZIALI:
 
+// Array di oggetti bicicletta
+const bikes = [
+    { name: 'Carrera', weight: 7 },
+    { name: 'Cinelli', weight: 6 },
+    { name: 'Cipollini', weight: 5 },
+    { name: 'Colnago', weight: 8 },
+  ];
+  
+// Variabile per peso minimo:
+let minWeightBike = bikes[0];
+
 //! LOGICA EFFETTIVA:
